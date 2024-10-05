db5a57ea3ce5a34742fb0ac78307840021f8244c
Bhuvanesh Duvvuri <duvvuribhuvanesh@zohomail.in>
2024-10-05 10:43:19 +0530

    Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-
-import "medhya/css/medhya.css";
+import "vedhya/css/vedhya.css";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const container = document.querySelector(".root")!;
 const root = createRoot(container);
 root.render(
